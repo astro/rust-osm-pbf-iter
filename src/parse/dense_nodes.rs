ea3a244889da29cdb7ec387c4d1605456184e21a
Astro <astro@spaceboyz.net>
2018-10-02 03:30:27 +0200

    dense_nodes: update style

--- src/parse/dense_nodes.rs
+++ src/parse/dense_nodes.rs
@@ -13,45 +13,26 @@ pub struct DenseNodesParser<'a> {
     keys_vals: DelimitedIter<'a, PackedVarint, u32>,
 }
 
-macro_rules! some {
-    ($e: expr) => {
-        match $e {
-            Some(x) => x,
-            None => return None,
-        }
-    }
-}
-
 impl<'a> DenseNodesParser<'a> {
     pub fn new(primitive_block: &'a PrimitiveBlock<'a>, data: &'a [u8]) -> Option<Self> {
         let iter = MessageIter::new(data);
 
-        // println!("keys_vals: {:?}", some!(iter.clone()
-        //               .tag::<ParseValue<'a>>(10)
-        //               .nth(0)).packed_varints::<u32>().collect::<Vec<u32>>());
-        // println!("infos: {:?}", iter.clone()
-        //          .tag::<ParseValue<'a>>(5)
-        //          .nth(0));
-
         Some(DenseNodesParser {
             primitive_block: primitive_block,
             ids: DeltaEncodedIter::new(
-                some!(iter.clone()
-                      .tag::<ParseValue<'a>>(1)
-                      .nth(0)
-                )
+                iter.clone()
+                    .tag::<ParseValue<'a>>(1)
+                    .nth(0)?
             ),
             lats: DeltaEncodedIter::new(
-                some!(iter.clone()
-                        .tag::<ParseValue<'a>>(8)
-                        .nth(0)
-                )
+                iter.clone()
+                    .tag::<ParseValue<'a>>(8)
+                    .nth(0)?
             ),
             lons: DeltaEncodedIter::new(
-                some!(iter.clone()
-                      .tag::<ParseValue<'a>>(9)
-                      .nth(0)
-                )
+                iter.clone()
+                    .tag::<ParseValue<'a>>(9)
+                    .nth(0)?
             ),
             infos: iter.clone()
                 .tag::<ParseValue<'a>>(5)
@@ -60,10 +41,9 @@ impl<'a> DenseNodesParser<'a> {
                      DenseInfoParser::new(&primitive_block, *value)
                 ),
             keys_vals: DelimitedIter::new(
-                some!(iter.clone()
-                      .tag::<ParseValue<'a>>(10)
-                      .nth(0)
-                )
+                iter.clone()
+                    .tag::<ParseValue<'a>>(10)
+                    .nth(0)?
             ),
         })
     }
@@ -73,7 +53,7 @@ impl<'a> Iterator for DenseNodesParser<'a> {
     type Item = Node<'a>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let mut keys_vals = some!(self.keys_vals.next()).into_iter();
+        let mut keys_vals = self.keys_vals.next()?.into_iter();
         let mut tags = Vec::with_capacity(keys_vals.clone().count());
         loop {
             let k = match keys_vals.next() {
@@ -93,12 +73,12 @@ impl<'a> Iterator for DenseNodesParser<'a> {
             .and_then(|infos| infos.next());
 
         Some(Node {
-            id: some!(self.ids.next()) as u64,
+            id: self.ids.next()? as u64,
             lat: self.primitive_block.convert_lat(
-                some!(self.lats.next())
+                self.lats.next()?
             ),
             lon: self.primitive_block.convert_lon(
-                some!(self.lons.next())
+                self.lons.next()?
             ),
             info: info,
             tags: tags,
